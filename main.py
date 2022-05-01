9a630acd03434331bcaa3a99c5df569d2ee3b670
ElliKraft <sunnyelya@gmail.com>
2022-05-01 23:31:16 +0200

    user input works, false classified cases from user input are saved to the case base

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-from scipy.spatial.distance import pdist, squareform, cdist
-import numpy
-import sys
+from scipy.spatial.distance import cdist
+
 
 import warnings
 warnings.filterwarnings("ignore")
 
-
+# Converts string values from columns Zimmerzahl, Stockwerk, Alter and Kaution to Float
 def convert_string_values(cases):
     cases.replace("3-4 Zimmer", 3.5, inplace=True, regex=True)
     cases.replace("4-5 Zimmer", 4.5, inplace=True, regex=True)
@@ -49,24 +47,24 @@ def convert_string_values(cases):
     cases.replace("1000-1500", 1250, inplace=True, regex=True)
     cases.replace("ueber 3000", 3000, inplace=True, regex=True)
 
-
+# Converts binary values from columns Hausmeister, Garage, Aufzug, Balkon, Terrasse, Kehrwoche and Moebliert into True and False
 def convert_binary_values(cases):
     cases.replace("ja", 1, inplace=True, regex=True)
     cases.replace("nein", 0, inplace=True, regex=True)
     cases.replace("teilmoebliert", 1, inplace=True, regex=True)
 
-
+# Applies one-hot-encoding on the columns Heizung, Kindergarten, Schule, S-Bahn, Lage, Kueche and Bad
 def one_hot_encoding(cases):
     heizung_dummies = pd.get_dummies(cases['Heizung'])
     merged = pd.concat([cases, heizung_dummies], axis='columns')
     cases = merged
-    kindergarten_dummies = pd.get_dummies(cases['Kindergarten'])
+    kindergarten_dummies = pd.get_dummies(cases['Kindergarten'],prefix='kindergarten')
     merged = pd.concat([cases, kindergarten_dummies], axis='columns')
     cases = merged
-    schule_dummies = pd.get_dummies(cases['Schule'])
+    schule_dummies = pd.get_dummies(cases['Schule'],prefix='schule')
     merged = pd.concat([cases, schule_dummies], axis='columns')
     cases = merged
-    sbahn_dummies = pd.get_dummies(cases['S-Bahn'])
+    sbahn_dummies = pd.get_dummies(cases['S-Bahn'],prefix='s-bahn')
     merged = pd.concat([cases, sbahn_dummies], axis='columns')
     cases = merged
     lage_dummies = pd.get_dummies(cases['Lage'])
@@ -86,137 +84,135 @@ def one_hot_encoding(cases):
         cases['Altersklasse'] = df2
     return cases
 
-
+# Builds the case base based on the given data
 def build_case_base(case_base_copy, cases, case_base):
-
-    print("CASES")
-    print(cases.head(50))
 
     for index, row in cases_copy.iterrows():
         if index < 66:
             row_distances = cdist(case_base_copy.values, np.array([cases_copy_ST.iloc[index+1]]), metric='euclidean')
 
-            print(index)
-            print(row_distances)
             min_distance_idx = row_distances.argmin()
 
-            print(min_distance_idx)
+            # If false classified
             if cases['Bewohner'][index+1] != case_base['Bewohner'][min_distance_idx] or cases['Altersklasse'][index+1] != case_base['Altersklasse'][min_distance_idx]:
-                print('classified incorrect')
-                print(cases['Bewohner'][index+1])
-                print(cases['Bewohner'][min_distance_idx])
-                print(cases['Altersklasse'][index+1])
-                print(cases['Altersklasse'][min_distance_idx])
-                print("INDEX")
-                print(index + 1)
-                # If false classified
                 case_base_copy = case_base_copy.append(cases_copy_ST.iloc[index+1])
+
                 # add the case with "Bewohner" and "Altersklasse" to the case base for further classification of user input
-                case_base = case_base.append(cases.iloc[index+1])
+                case_base = case_base.append(cases.iloc[index+1], ignore_index=True)
 
+    return case_base_copy, case_base
 
-            if cases['Bewohner'][index+1] == case_base['Bewohner'][min_distance_idx] and cases['Altersklasse'][index+1] == case_base['Altersklasse'][min_distance_idx]:
-                print('classified correct')
-                print(cases['Bewohner'][index+1])
-                print(cases['Bewohner'][min_distance_idx])
-                print(cases['Altersklasse'][index+1])
-                print(cases['Altersklasse'][min_distance_idx])
-
-    print(case_base_copy.shape)
-    return case_base_copy
-
-def allow_user_requests(case_base_copy):
-# def allow_user_requests():
+# Takes and encodes user input
+def allow_user_requests():
     user_input_df = pd.DataFrame(columns=['Zimmerzahl', 'Stockwerk', 'Heizung', 'Hausmeister', 'Kindergarten',
                                           'Schule', 'S-Bahn', 'Garage', 'Alter', 'Aufzug', 'Lage', 'Kaution',
                                           'Kueche', 'Bad', 'Balkon', 'Terrasse', 'Kehrwoche', 'Moebliert',
                                           'Miete', 'Nebenkosten', 'Quadratmeter'])
-    print(user_input_df)
 
-    # print('Geben Sie bitte die Informationen zu der Wohnung ein.')
-    # Zimmerzahl = str(input("Zimmerzahl: ") or "0")
-    # Stockwerk = str(input("Stockwerk: ") or "0")
-    # Heizung = str(input("Heizung: ") or "0")
-    # Hausmeister = str(input("Hausmeister: ") or "0")
-    # Kindergarten = str(input("Kindergarten: ") or "0")
-    # Schule = str(input("Schule: ") or "0")
-    # SBahn = str(input("S-Bahn: ") or "0")
-    # Garage = str(input("Garage: ") or "0")
-    # Alter = str(input("Alter: ") or "0")
-    # Aufzug = str(input("Aufzug: ") or "0")
-    # Lage = str(input("Lage: ") or "0")
-    # Kaution = str(input("Kaution: ") or "0")
-    # Kueche = str(input("Kueche: ") or "0")
-    # Bad = str(input("Bad: ") or "0")
-    # Balkon = str(input("Balkon: ") or "0")
-    # Terrasse = str(input("Terrasse: ") or "0")
-    # Kehrwoche = str(input("Kehrwoche: ") or "0")
-    # Moebliert = str(input("Moebliert: ") or "0")
-    # Miete = float(input("Miete: ") or 0)
-    # Nebenkosten = float(input("Nebenkosten: ") or 0)
-    # Quadratmeter = float(input("Quadratmeter: ") or 0)
+    print('Geben Sie bitte die Informationen zu der Wohnung ein.')
+    Zimmerzahl = str(input("Zimmerzahl: ") or "0")
+    Stockwerk = str(input("Stockwerk: ") or "0")
+    Heizung = str(input("Heizung: ") or "Oel")
+    Hausmeister = str(input("Hausmeister: ") or "0")
+    Kindergarten = str(input("Kindergarten: ") or "fern")
+    Schule = str(input("Schule: ") or "fern")
+    SBahn = str(input("S-Bahn: ") or "erreichbar")
+    Garage = str(input("Garage: ") or "0")
+    Alter = str(input("Alter: ") or "0")
+    Aufzug = str(input("Aufzug: ") or "0")
+    Lage = str(input("Lage: ") or "Nebenstrasse")
+    Kaution = str(input("Kaution: ") or "0")
+    Kueche = str(input("Kueche: ") or "Kueche (alt)")
+    Bad = str(input("Bad: ") or "Dusche")
+    Balkon = str(input("Balkon: ") or "0")
+    Terrasse = str(input("Terrasse: ") or "0")
+    Kehrwoche = str(input("Kehrwoche: ") or "0")
+    Moebliert = str(input("Moebliert: ") or "0")
+    Miete = float(input("Miete: ") or 0)
+    Nebenkosten = float(input("Nebenkosten: ") or 0)
+    Quadratmeter = float(input("Quadratmeter: ") or 0)
 
-    # user_input = [Zimmerzahl, Stockwerk, Heizung, Hausmeister, Kindergarten, Schule, SBahn, Garage, Alter,
-    #               Aufzug, Lage, Kaution, Kueche, Bad, Balkon, Terrasse, Kehrwoche, Moebliert, Miete, Nebenkosten,
-    #               Quadratmeter]
+    user_input = [Zimmerzahl, Stockwerk, Heizung, Hausmeister, Kindergarten, Schule, SBahn, Garage, Alter,
+                  Aufzug, Lage, Kaution, Kueche, Bad, Balkon, Terrasse, Kehrwoche, Moebliert, Miete, Nebenkosten,
+                  Quadratmeter]
 
-    user_input = ["5 Zimmer", "EG", "Oel", "ja", "nah", "nah", "nah", "ja", "51-75 Jahre",
-                    "ja", "Spielstrasse", "keine", "keine", "Dusche", "ja", "nein", "ja", "nein", 900, 80, 90]
-
+    # save user_input as row in user_input dataframe
     user_input_df.loc[len(user_input_df)] = user_input
-    print("DF with user input: ")
-    print(user_input_df)
 
     convert_binary_values(user_input_df)
     convert_string_values(user_input_df)
     user_input_df = one_hot_encoding(user_input_df)
 
-    print("Konvertierte Werte")
-    print(user_input_df)
-
-    #user_input_df_ST = np.array(user_input_df)
     user_input_ST = user_input_df.to_numpy().reshape(-1, 1)
 
-    print("Numpy")
-    print(user_input_ST.ndim)
-    standard_scaler = StandardScaler()
-    user_input_ST = standard_scaler.fit_transform(user_input_ST)
-    print("Skalierte Werte")
-    print(user_input_ST)
-
     user_input_ST = np.append(user_input_ST, [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0],
-                              [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
+                                              [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
     user_input_ST = user_input_ST.reshape(-1, 43)
-    print("Dimension")
-    print(user_input_ST)
+    return user_input_ST, user_input_df
+
+# Takes the scaled and original user inputs to classify it based on the built case base
+def classify_user_input(user_input_ST, user_input_df):
+    user_input_ST = np.multiply(user_input_ST, feature_importance_user_input)
 
     distance_for_user_input = cdist(case_base_copy.values, user_input_ST, metric='euclidean')
-    print("Distance for user input")
-    print(distance_for_user_input)
+
     min_distance_idx = distance_for_user_input.argmin()
-    print("Minimal distance for user input")
-    print(min_distance_idx)
-    print(cases['Bewohner'][min_distance_idx])
-    print(cases['Altersklasse'][min_distance_idx])
+
+    print(case_base['Bewohner'][min_distance_idx])
+    print(case_base['Altersklasse'][min_distance_idx])
+
+    korrekt_klassifiziert = str(input("War die Klassifizierung korrekt (Ja/Nein)? "))
+
+    # If not classified correctly, the case needs to be saved in the case base
+    if korrekt_klassifiziert == 'Nein':
+        case_base_local, case_base_final = save_user_input_in_case_base(user_input_ST, user_input_df, case_base)
+        return case_base_local, case_base_final
+
+    # Otherwise, it doesn't need to be saved
+    return case_base, case_base_copy
 
 
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.width', None)
-# pd.set_option('display.max_colwidth', None)
+
+# Saves the given user input as case in the case base
+def save_user_input_in_case_base(user_input_ST, user_input_df, case_base):
+    bewohner = str(input("Bitte geben Sie die richtige Klassifierung für Bewohner an: "))
+    altersklasse = str(input("Bitte geben Sie die richtige Klassifierung für Altersklasse an: "))
+    user_input_df['Bewohner'] = bewohner
+    user_input_df['Altersklasse'] = altersklasse
+    pd.DataFrame(data=user_input_df).transpose()
+
+    user_input_df_to_append = case_base[0:0]
+    transformed_df = pd.concat([user_input_df_to_append, user_input_df], axis=0, ignore_index=True)
+    parser = pd.io.parsers.base_parser.ParserBase({'usecols': None})
+    transformed_df.columns = parser._maybe_dedup_names(transformed_df.columns)
+    transformed_df = transformed_df.replace(np.nan, 0, regex=True)
+    case_base_local = case_base.append(transformed_df)
+
+    user_input_df_st = pd.DataFrame(user_input_ST, columns=case_base_copy.columns)
+    case_base_final = case_base_copy.append(user_input_df_st, ignore_index=True)
+
+    return case_base_local, case_base_final
+
+# To show the full dataframe in the console
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 
 # Reading the data into a data frame
 cases = pd.read_csv('A002.csv')
-# print(cases.head(20))
-# cases = cases.sample(frac=1).reset_index(drop=True)
-# print(cases.head(20))
 
-# Setting the importance value of features
-feature_importance = [0.9, 0.2, 0.4, 0.2, 0.1, 0.4, 0.4, 0.1, 0.1, 0.1, 0.9, 0.8, 0.95, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
-                      0.3, 0.3, 0.3, 0.7, 0.7, 0.7, 0.65, 0.65, 0.65, 0.5, 0.5, 0.5, 0.5, 0.5, 0.2, 0.2, 0.2, 0.4, 0.4,
-                      0.4, 0.4, 0.4, 1, 1]
+# Setting the importance value of features for given data
+feature_importance = [200, 0.2, 10, 0.2, 0.3, 10, 10, 0.1, 0.1, 0.1, 50, 200, 200, 200, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+                      0.3, 0.3, 0.3, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 10, 10,
+                      10, 10, 10]
 
+# Importance values for user input
+feature_importance_user_input = [200, 0.2, 10, 0.2, 0.3, 10, 10, 0.1, 0.1, 0.1, 50, 200, 200, 200, 0.1, 0.3,
+                                 50, 50, 50, 50, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+# Converting values into float and integer, to be able to calculate distance later
 convert_string_values(cases)
 convert_binary_values(cases)
 cases = one_hot_encoding(cases)
@@ -231,33 +227,33 @@ case_base_copy = case_base.drop(columns=['Bewohner', 'Altersklasse'])
 # Standardizing values
 cases_copy_ST = cases_copy.to_numpy()
 standard_scaler = StandardScaler()
-# standard_scaler = MinMaxScaler()
 cases_copy_ST = standard_scaler.fit_transform(cases_copy_ST)
+
 
 cases_copy_ST = case_base_copy.append(pd.DataFrame(cases_copy_ST, columns=list(case_base_copy)), ignore_index=True)
 case_base_copy = case_base_copy.append(cases_copy_ST.iloc[0])
 case_base = case_base.append(cases.iloc[0])
-print("Case base with one case from cases")
-print(case_base)
 
 cases_copy_ST = cases_copy_ST.multiply(feature_importance, axis='columns')
 
-print("case base copy before method")
-print('case_base_copy')
-print(case_base_copy)
-print('cases_copy')
-print(cases_copy)
-print('cases_copy_ST')
-print(cases_copy_ST)
-
-case_base_copy = build_case_base(case_base_copy, cases, case_base)
+case_base_copy, case_base = build_case_base(case_base_copy, cases, case_base)
 print("Fallbasis aufgebaut")
+print(case_base)
+case_base_copy = case_base_copy.reset_index(drop=True)
+print("Fallbasis skaliert")
 print(case_base_copy)
 
-allow_user_requests(case_base_copy)
+# User can give input as often as he likes
+nochmal_eingeben = True
+while nochmal_eingeben:
+    user_input_ST, user_input_df = allow_user_requests()
+    user_input_ST = standard_scaler.transform(user_input_ST)
+    case_base_final, case_base_copy_final = classify_user_input(user_input_ST, user_input_df)
+    nochmal_eingeben = str(input("Wollen Sie noch einmal eingeben? (Ja/Nein) "))
+    if nochmal_eingeben == 'Nein' or nochmal_eingeben == 'nein':
+        nochmal_eingeben = False
+        print('Finale Fallbasis')
+        print(case_base_final.reset_index(drop=True))
+        # print('Skalierte Fallbasis')
+        # print(case_base_copy_final)
 
-# for k in feature_importance.keys():
-#    print(feature_importance[k])
-
-
-# print(distance.euclidean())
